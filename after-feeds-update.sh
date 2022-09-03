b999888867d686ab97ce3f091482b6cfdf3d6192
J0EST1N <dyx4096@gmail.com>
2022-09-03 13:22:00 +0800

    Update after-feeds-update.sh

--- after-feeds-update.sh
+++ after-feeds-update.sh
@@ -12,4 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify default theme to argonne
+sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
