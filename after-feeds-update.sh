6e93072fe74d26a14bb1c64e908e65bcd4062169
J0EST1N <dyx4096@gmail.com>
2022-09-04 15:01:50 +0800

    Update after-feeds-update.sh

--- after-feeds-update.sh
+++ after-feeds-update.sh
@@ -14,4 +14,4 @@
 #sed -i 's/192.168.1.1/192.168.84.42/g' package/base-files/files/bin/config_generate
 
 # Modify default theme to argonne
-sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-neobird/g' feeds/luci/collections/luci/Makefile
